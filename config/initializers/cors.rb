28834911d3be560232859eb68943236f051e2535
Mayowa Pitan <mayowa.pitan@andela.com>
2019-12-17 17:11:47 +0100

    update rack config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,8 +9,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ENV['IDEABOARD_DOMAIN']
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
